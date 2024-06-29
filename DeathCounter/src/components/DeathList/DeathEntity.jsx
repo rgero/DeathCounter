827839d759a3ab0cee641525d06e54bc029e0737
Roy Gero <roy.gero.ii@gmail.com>
2024-06-29 13:30:07 -0400

    Adding some styling to show when the object is hovered over.
    This needs to be worked on a lot, but will be the beginning of the "edit" functionality.

--- DeathCounter/src/components/DeathList/DeathEntity.jsx
+++ DeathCounter/src/components/DeathList/DeathEntity.jsx
@@ -1,8 +1,16 @@
 import { Grid, Typography } from "@mui/material"
 
+const styles = {
+  gridItem: {
+    '&:hover': {
+      background: "#f00",
+    }
+  }
+}
+
 const DeathEntity = ({children}) => {
   return (
-    <Grid container spacing={5}>
+    <Grid container spacing={5} onClick={()=> alert(`Hello ${children.name}`)} sx={styles.gridItem}>
       <Grid item>
         <Typography variant="h5">
           {children.name}
