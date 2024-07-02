1ce9bda552bf3eab09633a54288d7edfe52cf027
Roy Gero <roy.gero.ii@gmail.com>
2024-07-02 14:08:35 -0400

    Setting Dark Mode.

--- DeathCounter/src/App.jsx
+++ DeathCounter/src/App.jsx
@@ -1,16 +1,26 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { CssBaseline, ThemeProvider, createTheme } from "@mui/material"
 
 import DeathPage from "./page/DeathPage"
 import { SocketProvider } from "./context/WebSocketContext"
 
 function App() {
+  const darkTheme = createTheme({
+    palette: {
+      mode: 'dark',
+    },
+  });
+
   return (
     <SocketProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route index element={<DeathPage/>}/>
-        </Routes>
-      </BrowserRouter>
+      <ThemeProvider theme={darkTheme}>
+        <CssBaseline/>
+        <BrowserRouter>
+          <Routes>
+            <Route index element={<DeathPage/>}/>
+          </Routes>
+        </BrowserRouter>
+      </ThemeProvider>
     </SocketProvider>
   )
 }
