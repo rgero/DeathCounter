b2fff97b54960e28b3a5ca63709e350933c0b1a6
Roy Gero <roy.gero.ii@gmail.com>
2024-07-05 11:32:25 -0400

    Adding a pie chart for the deaths

---
+++ DeathCounter/src/components/Stats/DeathPieChart.jsx
@@ -0,0 +1,49 @@
+import { Pie, PieChart } from "recharts";
+
+import { Box } from "@mui/material";
+import { useDeathTracker } from "../../context/DeathTrackerContext";
+
+const DeathPieChart = () => {
+  const { deathList } = useDeathTracker();
+
+  const dimension = 250;
+  const filteredItems = deathList.filter(item => item.deaths !== 0);
+
+
+  const renderLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, value, index}) => {
+    const RADIAN = Math.PI / 180;
+    const radius = 25 + innerRadius + (outerRadius - innerRadius);
+    const x = cx + radius * Math.cos(-midAngle * RADIAN);
+    const y = cy + radius * Math.sin(-midAngle * RADIAN);
+
+    return (
+      <text
+        x={x}
+        y={y}
+        fill="#ac3232"
+        textAnchor={x > cx ? "start" : "end"}
+        dominantBaseline="central"
+      >
+        {deathList[index].name}
+      </text>
+    );
+  }
+
+  return (
+    <Box display="flex" justifyContent="center">
+      <PieChart width={dimension*2} height={dimension}>
+        <Pie
+          dataKey="deaths"
+          data={filteredItems}
+          cx="50%"
+          cy="50%"
+          outerRadius={80}
+          fill="#ac3232"
+          label={renderLabel}
+        />
+      </PieChart>
+    </Box>
+  )
+}
+
+export default DeathPieChart
--- DeathCounter/src/components/Stats/Stats.jsx
+++ DeathCounter/src/components/Stats/Stats.jsx
@@ -1,5 +1,6 @@
-import { Container, Typography } from "@mui/material";
+import { Container, Grid, Typography } from "@mui/material";
 
+import DeathPieChart from "./DeathPieChart";
 import { useDeathTracker } from "../../context/DeathTrackerContext";
 
 const Stats = () => {
@@ -8,13 +9,20 @@ const Stats = () => {
   const sortedList = deathList.sort( (a,b) => b.deaths - a.deaths);
   const totalDeaths = deathList.reduce((total, currentItem) => total + currentItem.deaths, 0);
   const averagePerBoss = Math.round(totalDeaths / deathList.length * 100, 2)/100;
-  
 
   return (
-    <Container sx={{paddingTop: 2}}>
-      <Typography>Stats</Typography>
-      <Typography>Average Per Boss: {averagePerBoss}</Typography>
-      <Typography>Most Deaths: {sortedList[0].name} - {sortedList[0].deaths}</Typography>
+    <Container sx={{paddingTop: 5}}>
+        <Typography variant="h4">Stats</Typography>
+        <Grid container direction="row" alignItems="center" justifyContent="space-around">
+          <Grid item>
+            <Typography>Average Per Boss: {averagePerBoss}</Typography>
+            <Typography>Most Deaths: {sortedList[0].name} - {sortedList[0].deaths}</Typography>
+          </Grid>
+          <Grid item>
+            <DeathPieChart/>
+          </Grid>
+        </Grid>
+
     </Container>
   )
 }
