ea0f677bf2a15febe302b0d8ac4036698803f32d
Roy Gero <roy.gero.ii@gmail.com>
2025-06-17 09:11:08 -0400

    Added the header background back
    I need to make a new one that it is a little taller so it fits better.

--- DeathCounter 2.0/src/components/header/HeaderBar.tsx
+++ DeathCounter 2.0/src/components/header/HeaderBar.tsx
@@ -12,41 +12,51 @@ const HeaderBar = () => {
   const isMobile = useMediaQuery(theme.breakpoints.down("md"));
 
   return (
-    <AppBar position="static" 
-      sx={{ 
-        px: 2, 
-        padding: "0.75rem",
+    <Box
+      sx={{
+        position: "relative",
+        backgroundImage: 'url("/Header.svg")',
+        backgroundRepeat: 'repeat-x',
       }}
     >
-      <Grid container justifyContent="space-between" alignItems="center">
-        <Grid>
-          <Link to="/" style={{ display: "flex", alignItems: "center", textDecoration: "none", color: "inherit" }}>
-            <Box sx={{
-              width: { xs: "24px", md: "32px" },
-              height: { xs: "24px", md: "32px" },
-              marginRight: "0.5rem"
-            }}>
-              <img 
-                src="/logo.svg" 
-                alt="Death Counter" 
-                style={{ width: "100%", height: "100%" }} // Ensure img fills the Box
-              />
-            </Box>
-            <Typography variant="h5">The Death Counter</Typography>
-          </Link>
-        </Grid>
-        {user && (
-          <Grid container alignItems="center" justifyContent="flex-end" spacing={2}>
-            {!isMobile && (<Grid>
-              <GameSelector/>
-            </Grid>)}
-            <Grid>
-              <UserAvatar />
-            </Grid>
+      <AppBar position="static" 
+        sx={{ 
+          px: 2, 
+          padding: "0.5rem",
+          backgroundColor: "transparent",
+        }}
+        elevation={0}
+      >
+        <Grid container justifyContent="space-between" alignItems="center">
+          <Grid>
+            <Link to="/" style={{ display: "flex", alignItems: "center", textDecoration: "none", color: "inherit" }}>
+              <Box sx={{
+                width: { xs: "24px", md: "32px" },
+                height: { xs: "24px", md: "32px" },
+                marginRight: "0.5rem"
+              }}>
+                <img 
+                  src="/logo.svg" 
+                  alt="Death Counter" 
+                  style={{ width: "100%", height: "100%" }} // Ensure img fills the Box
+                />
+              </Box>
+              <Typography variant="h5">The Death Counter</Typography>
+            </Link>
           </Grid>
-        )}
-      </Grid>
-    </AppBar>
+          {user && (
+            <Grid container alignItems="center" justifyContent="flex-end" spacing={2}>
+              {!isMobile && (<Grid>
+                <GameSelector/>
+              </Grid>)}
+              <Grid>
+                <UserAvatar />
+              </Grid>
+            </Grid>
+          )}
+        </Grid>
+      </AppBar>
+    </Box>
   )
 }
 
