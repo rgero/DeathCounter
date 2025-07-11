38496745dd80537ec65e0456895f3b615f277837
Roy Gero <roy.gero.ii@gmail.com>
2025-07-10 21:58:04 -0400

    Fixing the issue where I was briefly showing the encrypted data.

--- DeathCounter 2.0/src/utils/crypt.ts
+++ DeathCounter 2.0/src/utils/crypt.ts
@@ -3,7 +3,7 @@ import { DeathList } from "../interfaces/DeathList";
 
 const SECRET_KEY = import.meta.env.VITE_SECRET_KEY;
 
-const encryptField = (text: string|undefined): string => {
+const encryptField = (text: string | undefined): string => {
   if (!text) return "";
   return CryptoJS.AES.encrypt(text, SECRET_KEY).toString();
 };
@@ -15,25 +15,25 @@ const decryptField = (ciphertext: string): string => {
 };
 
 export const decryptData = (data: DeathList): DeathList => {
-  data.name = decryptField(data.name)
-  data.description = data.description ? decryptField(data.description) : "";
-
-  data.entityList = data.entityList.map((entity) => ({
-    ...entity,
-    name: decryptField(entity.name),
-  }));
-
-  return data;
-}
+  return {
+    ...data,
+    name: decryptField(data.name),
+    description: data.description ? decryptField(data.description) : "",
+    entityList: data.entityList.map((entity) => ({
+      ...entity,
+      name: decryptField(entity.name),
+    })),
+  };
+};
 
 export const encryptData = (data: DeathList): DeathList => {
-  data.name = encryptField(data.name);
-  data.description = data.description ? encryptField(data.description) : "";
-
-  data.entityList = data.entityList.map((entity) => ({
-    ...entity,
-    name: encryptField(entity.name),
-  }));
-
-  return data;
-}
+  return {
+    ...data,
+    name: encryptField(data.name),
+    description: data.description ? encryptField(data.description) : "",
+    entityList: data.entityList.map((entity) => ({
+      ...entity,
+      name: encryptField(entity.name),
+    })),
+  };
+};
