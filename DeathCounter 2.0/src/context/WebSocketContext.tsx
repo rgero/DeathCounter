295c3a8fedec9d1b5cb3df98ef15437fb3cf35db
Roy Gero <roy.gero.ii@gmail.com>
2025-07-13 21:53:24 -0400

    Refactoring the socket provider so it has a similar approach to Fajita.

--- DeathCounter 2.0/src/context/WebSocketContext.tsx
+++ DeathCounter 2.0/src/context/WebSocketContext.tsx
@@ -1,23 +1,43 @@
-import { createContext, useContext } from "react";
+import { createContext, useCallback, useContext, useEffect, useState } from "react";
+import io, { Socket } from 'socket.io-client';
 
-import io from 'socket.io-client';
+import { useDeathLists } from "./DeathCounterContext";
 
-const socket = io(`${import.meta.env.VITE_WSS_URL}`, {
-    withCredentials: true,
-    transports: ['websocket', 'polling'],
-    upgrade: true,
-    rememberUpgrade: true
-});
+interface SocketContextType {
+  socket: Socket | undefined,
+}
 
-const SocketContext = createContext(socket);
+const SocketContext = createContext<SocketContextType | undefined>(undefined);
 
 export const SocketProvider: React.FC<{ children: React.ReactNode }> = ({children}) => {
+  const [socket, setSocket] = useState<Socket | undefined>(undefined);
+  const {getCurrentlyActiveDeathList} = useDeathLists();
+
+  const initializeSocket = useCallback((params: Record<string, string>) => {
+    const newSocket = io(`${import.meta.env.VITE_WSS_URL}`, {
+      withCredentials: true,
+      query: params,
+    });
+    setSocket(newSocket);
+  }, []);
+
+  useEffect(() => {
+    const activeDeathList = getCurrentlyActiveDeathList();
+    if (!activeDeathList || !activeDeathList.id) return;
+    if (socket) {
+      socket.disconnect();
+    }
+    
+    initializeSocket({token: activeDeathList.token ?? ""});
+  }, [getCurrentlyActiveDeathList, initializeSocket, socket]);
+
+
   console.log("SocketProvider initialized");
   return (
     <SocketContext.Provider 
-      value={
+      value={{
         socket
-      }
+      }}
     >
       {children}
     </SocketContext.Provider>
