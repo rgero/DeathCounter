7442c03540c04930a4a10571952f8aa9d6990013
Roy Gero <roy.gero.ii@gmail.com>
2025-07-18 16:34:00 -0400

    Removing the old Death Counter and migrating the folder.

--- DeathCounter/src/utils/isMobile.ts
+++
@@ -1,6 +0,0 @@
-import { useMediaQuery, useTheme } from "@mui/material";
-
-export const isMobile = (): boolean => {
-  const theme = useTheme();
-  return useMediaQuery(theme.breakpoints.down("md"));
-}
