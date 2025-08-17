b4be42cc2856a07423411bc17a74832da0205515
Roy Gero <roy.gero.ii@gmail.com>
2025-08-17 19:01:51 -0400

    Adding an option to import data on the main screen.

--- DeathCounter/DeathCounter/src/components/death_list/NoDeathListFound.tsx
+++ DeathCounter/DeathCounter/src/components/death_list/NoDeathListFound.tsx
@@ -1,11 +1,11 @@
+import { Add, Upload } from "@mui/icons-material"
 import { Box, Container, Divider, Typography } from "@mui/material"
 
-import { Add } from "@mui/icons-material"
 import Button from "../ui/Button";
 import { useModalProvider } from "../../context/modal/ModalContext";
 
 const NoDeathListFound = () => {
-  const {toggleCreateNewModal} = useModalProvider();
+  const {toggleCreateNewModal, toggleImportModal} = useModalProvider();
   return (
     <Container>
       <Typography variant="h4">Dashboard</Typography>
@@ -13,11 +13,15 @@ const NoDeathListFound = () => {
       <Box sx={{textAlign: "center", paddingTop: "20px"}}>
         <Typography variant="h5">No active death counter found.</Typography>
         <Typography variant="body1">Please create a new death counter to start tracking.</Typography>
-        <Box sx={{textAlign: "center", paddingTop: "30px"}}>
+        <Box sx={{textAlign: "center", paddingTop: "30px", gap:5, display: "flex", justifyContent: "center"}}>
           <Button
             icon={<Add />}
-            onClick={() => toggleCreateNewModal()}
-            title="Create New"/>
+            onClick={toggleCreateNewModal}
+            title="Create"/>
+          <Button
+            icon={<Upload/>}
+            onClick={toggleImportModal}
+            title="Import"/>
         </Box>
       </Box>
     </Container>
