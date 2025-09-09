63c8e257275a6b515fb1367d5a3e7691d39c210a
Roy Gero <roy.gero.ii@gmail.com>
2025-09-09 19:40:51 -0400

    Stopping the title bar from using two lines on mobile

--- DeathCounter/DeathCounter/src/components/death_list/DeathlistHeader.tsx
+++ DeathCounter/DeathCounter/src/components/death_list/DeathlistHeader.tsx
@@ -6,7 +6,13 @@ import { Info } from "@mui/icons-material";
 import { useDeathLists } from "../../context/deathCounter/DeathCounterContext";
 
 const DeathlistHeader = () => {
-  const { activeDeathList, isLoading, updateDeathList, showDescription, toggleDescription } = useDeathLists();
+  const {
+    activeDeathList,
+    isLoading,
+    updateDeathList,
+    showDescription,
+    toggleDescription,
+  } = useDeathLists();
 
   const [name, setName] = useState("");
   const inputRef = useRef<HTMLInputElement>(null);
@@ -19,23 +25,29 @@ const DeathlistHeader = () => {
 
   const handleSubmit = async () => {
     if (!activeDeathList || name === activeDeathList.name) return;
-
     activeDeathList.name = name;
     await updateDeathList(activeDeathList);
   };
 
   const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
     if (e.key === "Enter") {
-      e.preventDefault(); // Prevent newline or form submit
-      inputRef.current?.blur(); // Force blur to trigger handleSubmit
+      e.preventDefault();
+      inputRef.current?.blur();
     }
   };
 
   if (isLoading || !activeDeathList) return null;
 
   return (
-    <Grid container direction="row" justifyContent="space-between" alignItems="center">
-      <Grid>
+    <Grid
+      container
+      direction="row"
+      justifyContent="space-between"
+      alignItems="center"
+      wrap="nowrap"
+      sx={{paddingBottom: 2}}
+    >
+      <Grid sx={{ flexGrow: 1, minWidth: 0 }}>
         <TextField
           inputRef={inputRef}
           variant="standard"
@@ -43,7 +55,7 @@ const DeathlistHeader = () => {
           onChange={(e) => setName(e.target.value)}
           onBlur={handleSubmit}
           onKeyDown={handleKeyDown}
-          InputProps={{ disableUnderline: true }}
+          fullWidth
           sx={{
             fontSize: "2rem",
             fontWeight: 500,
@@ -56,15 +68,11 @@ const DeathlistHeader = () => {
           }}
         />
       </Grid>
-      <Grid>
-        <Grid container>
+      
+      <Grid sx={{ flexShrink: 0, ml: 1 }}>
+        <Grid container wrap="nowrap">
           <Grid>
-            <IconButton
-              aria-label="death-list-description"
-              aria-controls="death-list-description"
-              aria-haspopup="true"
-              onClick={toggleDescription}
-            >
+            <IconButton onClick={toggleDescription}>
               <Info color={showDescription ? "primary" : undefined} />
             </IconButton>
           </Grid>
