2d78d8482e5c8b6d08b4a706ee739c8c22e4fe37
Roy Gero <roy.gero.ii@gmail.com>
2025-09-24 22:08:57 -0400

    Throwing game stats under the description

--- DeathCounter/DeathCounter/src/components/death_list/DeathDescription.tsx
+++ DeathCounter/DeathCounter/src/components/death_list/DeathDescription.tsx
@@ -1,4 +1,4 @@
-import { Box, Collapse, TextField } from "@mui/material";
+import { Box, Collapse, Grid, TextField, Typography } from "@mui/material";
 import { useEffect, useState } from "react";
 
 import { grey } from "@mui/material/colors";
@@ -25,7 +25,7 @@ const DeathDescription = () => {
     <Collapse in={showDescription} timeout={500}>
       <Box sx={{
             backgroundColor: `${grey[900]}`,
-            padding: "10px",
+            padding: "20px",
             borderRadius: 5,
             boxShadow: 3,
             marginY: "10px",
@@ -44,6 +44,13 @@ const DeathDescription = () => {
           onBlur={handleBlur}
           sx={{marginY: "20px"}}
         />
+        <Box sx={{backgroundColor: `${grey[800]}`, padding: "10px", borderRadius: 3}}>
+          <Typography variant="h5" color="textSecondary">Stats</Typography>
+          <Grid container sx={{marginTop: "10px"}} direction="column">
+            <Grid><Typography>Total Bosses: {activeDeathList?.entityList.length || 0}</Typography></Grid>
+            <Grid><Typography>Total Deaths: {activeDeathList?.entityList.reduce((acc, entity) => acc + (entity.deaths || 0), 0) || 0}</Typography></Grid>
+          </Grid>
+        </Box>
       </Box>
     </Collapse>
   );
