dac120e85503230ee3f5005de2b136dd823ba8cc
Roy Gero <roy.gero.ii@gmail.com>
2025-11-10 23:31:11 -0500

    Adding highlighting to the list.

--- DeathCounter/DeathCounter/src/components/death_list/DeathEntity.tsx
+++ DeathCounter/DeathCounter/src/components/death_list/DeathEntity.tsx
@@ -3,30 +3,35 @@ import { Grid, Typography, useTheme } from "@mui/material"
 import { Entity } from '@interfaces/Entity';
 import { useDeathLists } from '@context/deathCounter/DeathCounterContext';
 
-const DeathEntity = ({index, data} : {index: number, data: Entity}) => {
-  const {setEntityInEdit} = useDeathLists();
+const DeathEntity = ({ index, data }: { index: number; data: Entity }) => {
+  const { setEntityInEdit } = useDeathLists();
   const theme = useTheme();
-  const styles = {
-    gridItem: {
-      paddingX: 1,
-      backgroundColor: index % 2 === 0 ? "" : theme.palette.grey[800]
-    }
-  }
 
   return (
-    <Grid container direction="row" justifyContent="space-between" sx={styles.gridItem} onClick={() => { setEntityInEdit(data) } }>
+    <Grid
+      container
+      direction="row"
+      justifyContent="space-between"
+      onClick={() => setEntityInEdit(data)}
+      sx={{
+        px: 1,
+        py: 0.5,
+        backgroundColor: index % 2 === 0 ? "transparent" : theme.palette.grey[900],
+        cursor: "pointer",
+        transition: "background-color 0.2s ease",
+        "&:hover": {
+          backgroundColor: theme.palette.action.hover,
+        },
+      }}
+    >
       <Grid size={10}>
-        <Typography>
-          {data.name}
-        </Typography>
+        <Typography>{data.name}</Typography>
       </Grid>
       <Grid size={2}>
-        <Typography align="right">
-          {data.deaths}
-        </Typography>
+        <Typography align="right">{data.deaths}</Typography>
       </Grid>
     </Grid>
-  )
-}
+  );
+};
 
-export default DeathEntity
+export default DeathEntity;
