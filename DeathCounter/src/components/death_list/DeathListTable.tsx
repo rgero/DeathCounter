2a4837152caf4f5d78180acaec0e038eaba58f19
Roy Gero <roy.gero.ii@gmail.com>
2026-05-18 21:00:41 -0400

    Fixing the weird gap between the death list and the entity form.

--- DeathCounter/src/components/death_list/DeathListTable.tsx
+++ DeathCounter/src/components/death_list/DeathListTable.tsx
@@ -1,4 +1,4 @@
-import { Container, Grid, Paper } from '@mui/material';
+import { Container, Paper } from '@mui/material';
 
 import DeathEntity from './DeathEntity';
 import DeathListTableHeader from './DeathListTableHeader';
@@ -16,24 +16,20 @@ const DeathListTable = () => {
 
   return (
     <Container disableGutters>
-      <Grid container spacing={2} direction="column">
-        <Grid>
-          <Paper sx={{
-            p: 2,
-            borderRadius: 5,
-            width: isMobile ? "90%" : "500px",
-            mx: "auto"
-          }}>
-            <DeathListTableHeader/>
-            {activeDeathList.entityList
-              .sort((a, b) => (a.id ?? 0) - (b.id ?? 0))
-              .map((item, index) => (
-                <DeathEntity key={index} data={item} index={index} />
-              ))
-            }
-          </Paper>
-        </Grid>
-      </Grid>
+      <Paper sx={{
+        p: 2,
+        borderRadius: 5,
+        width: isMobile ? "90%" : "500px",
+        mx: "auto"
+      }}>
+        <DeathListTableHeader/>
+        {activeDeathList.entityList
+          .sort((a, b) => (a.id ?? 0) - (b.id ?? 0))
+          .map((item, index) => (
+            <DeathEntity key={index} data={item} index={index} />
+          ))
+        }
+      </Paper>
     </Container>
   );
 };
